55d9317058f7686a9aad54ed9dbd3bf1c3872062
alcubierre-drive <alcubierre-drive>
2024-09-25 12:09:03 +0200

    Ofast results in SIGSEGV

--- config.mk
+++ config.mk
@@ -19,10 +19,10 @@ CC = gcc
 
 CFLAGS = -std=c11 -Wall -Wextra -pedantic -Wno-unused-parameter
 
-CFLAGS += -fsanitize=address
-LDFLAGS += -fsanitize=address
-CFLAGS += -g -ggdb
-CFLAGS += -Ofast -march=native -mtune=native -DNDEBUG -flto
-LDFLAGS += -flto=12
+# CFLAGS += -fsanitize=address
+# LDFLAGS += -fsanitize=address
+# CFLAGS += -g -ggdb
+CFLAGS += -O3 -march=native -mtune=native -DNDEBUG -flto
+LDFLAGS += -O3 -march=native -mtune=native -DNDEBUG -flto=12
 CFLAGS += -DUSLEEP_NOT_DEFINED \
  	  -DAWL_PULSEWIDGET_SINK=\"combine_sink\" -DAWL_PULSEWIDGET_HEAD
