e44afefeee8f1fc1b4168e151f86b8bcab753c10
alcubierre-drive <alcubierre-drive>
2024-11-08 10:42:23 +0100

    somehow we always get SEGV when not using sanitized addresses

--- config.mk
+++ config.mk
@@ -22,6 +22,5 @@ CFLAGS = -std=c11 -Wall -Wextra -pedantic -Wno-unused-parameter
 CFLAGS += -fsanitize=address -fsanitize=leak
 LDFLAGS += -fsanitize=address -fsanitize=leak
 CFLAGS += -g -ggdb
-CFLAGS += -Ofast -march=native -mtune=native -DNDEBUG -flto
-LDFLAGS += -Ofast -march=native -mtune=native -DNDEBUG -flto=12
+CFLAGS += -O3
 CFLAGS += -DUSLEEP_NOT_DEFINED
