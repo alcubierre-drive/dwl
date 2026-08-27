6d0dd46f248c25e4764c7940bb5b6b854a6c4d5c
alcubierre-drive <alcubierre-drive>
2026-08-27 14:16:37 +0200

    no warnings

--- tray/watcher.cpp
+++ tray/watcher.cpp
@@ -28,6 +28,7 @@ Watcher::~Watcher() {
 }
 
 void Watcher::busAcquired(const Glib::RefPtr<Gio::DBus::Connection>& conn, Glib::ustring name) {
+  (void)name;
   GError* error = nullptr;
   g_dbus_interface_skeleton_export(G_DBUS_INTERFACE_SKELETON(watcher_), conn->gobj(),
                                    "/StatusNotifierWatcher", &error);
@@ -148,6 +149,8 @@ Watcher::GfWatch* Watcher::gfWatchNew(GfWatchType type, const gchar* service, co
 }
 
 void Watcher::nameVanished(GDBusConnection* connection, const char* name, gpointer data) {
+  (void)connection;
+  (void)name;
   auto watch = static_cast<GfWatch*>(data);
   if (watch->type == GF_WATCH_TYPE_HOST) {
     watch->watcher->hosts_ = g_slist_remove(watch->watcher->hosts_, watch);
